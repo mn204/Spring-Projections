ab259bf0b2522fdb9b36e8b0f32d498bcd7e853e
Manuel <mln204manu@gmail.com>
2025-07-23 18:06:58 -0300

    primer commit

---
+++ src/main/java/com/pp/spring_projections/SpringProjectionsApplication.java
@@ -0,0 +1,14 @@
+package com.pp.spring_projections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectionsApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/pp/spring_projections/SpringProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.spring_projections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
